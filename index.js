f54c58bf9d60728a1000aec7a7ccb850ad3883d6
Umer Hassan <umerhassan94@gmail.com>
2019-06-15 21:09:11 -0400

    attempt to get jsx working with grids

--- index.js
+++ index.js
@@ -2,5 +2,20 @@
  * Created by umerhassan on 6/9/19.
  */
 const rootElement = document.getElementById('root');
-const element = <h1>Dummy Page</h1>
 ReactDOM.render(element, rootElement);
+class Grid extends React.Component {
+  render() {
+    return (
+      <div id="grid">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Grid/>, rootElement);
